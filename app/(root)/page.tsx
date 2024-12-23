8b9e066267c303078d878e189dbbf578bf241fcd
hisham rehab <hishamrehab309@gmail.com>
2024-12-23 22:10:28 +0200

    Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <>
-      <h1> Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup , <br />
+          Connect With Enterpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas</p>
+      </section>
     </>
   );
 }
